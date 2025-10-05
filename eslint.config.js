561c352aa5df49070bf4bb6e25eb6b2ff7efadfa
lupinus <bzirs@outlook.com>
2025-10-05 23:56:30 +0800

    :bug: fix: eslint 配置

--- eslint.config.js
+++ eslint.config.js
@@ -5,36 +5,55 @@ import jsxA11y from "eslint-plugin-jsx-a11y";
 import importPlugin from "eslint-plugin-import";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
+import reactRefresh from "eslint-plugin-react-refresh";
 
+// Reflect.deleteProperty(reactHooks.configs["recommended-latest"], "plugins");
+
+// 开发过程中在代码/页面 提示报错
 export default defineConfig([
   globalIgnores(["dist"]),
   js.configs.recommended,
+  reactRefresh.configs.next,
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
+
+    // plugins: {
+    //   "react-hooks": reactHooks,
+    // },
+    // extends: ["react-hooks/recommended-latest"],
+    extends: [reactHooks.configs["recommended-latest"]],
+
+    // ...reactHooks.configs["recommended-latest"],
+    // rules: {
+    // ...reactHooks.configs.recommended.rules,
+    // },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  {
+    files: ["src/**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       sourceType: "module",
       globals: {
         // https://eslint.org/docs/latest/use/configure/language-options#predefined-global-variables
+        // https://zh-hans.eslint.org/docs/latest/use/configure/language-options#%E6%8C%87%E5%AE%9A%E5%85%A8%E5%B1%80%E5%8F%98%E9%87%8F
         // 预定义全局变量
         ...globals.browser,
       },
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true, // 识别 jsx 语法
-        },
-      },
     },
-    // extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
     plugins: {
       react,
-      "react-hooks": reactHooks,
       "jsx-a11y": jsxA11y,
       import: importPlugin,
     },
     rules: {
+      ...react.configs.recommended.rules,
       "react/react-in-jsx-scope": "off", // Vite + React 不需要 import React
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
     },
     settings: {
       react: { version: "detect" },
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import eslint from "vite-plugin-eslint2";
 export default defineConfig({
   plugins: [
     react(),
+    // vite-plugin-eslint2 的核心功能就是在开发过程中发现 ESLint 检查到的问题，并在控制台提示报错或警告
     eslint({
       lintInWorker: true,
       lintOnStart: true,
