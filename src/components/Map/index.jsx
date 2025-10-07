4c6590cb8cfeeaaf41b6e859e03451689cc56c80
lupinus <bzirs@outlook.com>
2025-10-07 17:40:23 +0800

    :new: feat: worldwise 获取当前位置

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
 import styles from './index.module.css'
 import { useNavigate, useSearchParams } from 'react-router'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent, useMapEvents } from 'react-leaflet'
 import L from 'leaflet'
 import { useCitiesContext } from '../../context/CitiesContext.jsx'
+import { useGeolocation } from '../../hooks/useGeoLocation.js'
+import Button from '../Button/index.jsx'
 
 // 创建 L.Icon 实例
 const customIcon = L.icon({
@@ -15,14 +17,17 @@ const customIcon = L.icon({
 })
 
 function Index() {
-  const [searchParams, setSearchParams] = useSearchParams()
   // const position = useRef([40, 0])
-
+  const [searchParams, setSearchParams] = useSearchParams()
   const [map, setMap] = useState([40, 0])
+
+  const { isLoading: isPositionLoading, position: geoLocationPosition, getPosition } = useGeolocation()
 
   const { cities } = useCitiesContext()
 
-  const navigate = useNavigate()
+  function getGeoLocation() {
+    getPosition()
+  }
 
   useEffect(() => {
     const lat = +searchParams.get('lat')
@@ -32,8 +37,20 @@ function Index() {
     setMap([lat, lng])
   }, [searchParams])
 
+  useEffect(() => {
+    console.log(geoLocationPosition)
+    if (!geoLocationPosition) return
+
+    setMap([geoLocationPosition.lat, geoLocationPosition.lng])
+  }, [geoLocationPosition])
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate('add')}>
+    <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type="position" handler={getGeoLocation}>
+          {isPositionLoading ? 'Loading...' : 'USE YOUR POSITION'}
+        </Button>
+      )}
       <MapContainer center={map} zoom={13} scrollWheelZoom={true} className={styles.map}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -48,6 +65,7 @@ function Index() {
           </Marker>
         ))}
         <MoveCenter position={map} />
+        <AddToMap />
       </MapContainer>
     </div>
   )
@@ -57,6 +75,14 @@ function MoveCenter({ position }) {
   const map = useMap()
   map.setView(position)
   return null
+}
+
+function AddToMap() {
+  const navigate = useNavigate()
+
+  useMapEvents({
+    click: e => navigate(`add?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  })
 }
 
 export default Index
